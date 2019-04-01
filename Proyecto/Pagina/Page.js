92d6d6b8f44281d94d72cc4f575c80fcbb6fa783
dd <btorres20612@universidadean.edu.co>
2019-04-01 13:57:19 -0500

    navbar responsive

--- Proyecto/Pagina/Page.js
+++ Proyecto/Pagina/Page.js
@@ -1,13 +1,3 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-        /*   */
-function myFunction() {
-   var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
 
 function validar() {
    var expresion2 =/\w+@\w+\.+[a-z]/;
