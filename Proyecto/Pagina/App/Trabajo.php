d189a283fa6474df261cd31cf72aff324fdf2435
dd <btorres20612@universidadean.edu.co>
2019-05-06 15:58:54 -0500

    goglgoglgoglgogl

--- Proyecto/Pagina/App/Trabajo.php
+++ Proyecto/Pagina/App/Trabajo.php
@@ -2,12 +2,50 @@
 <html lang="es">
 <head>
 
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="AppC.css" type="text/css">
+    <script type="text/javascript" src="Page.js"></script>
     <title>App</title>
 </head>
 
 <body>
 
+    <!-- navbar -->
+    <header class="nav_bar fixed-top">
 
+        <div class="menu_bar">
+            <p href="#" class="boton_menu"><span class="icon-menu"></span>Menu</p>
+        </div>
+
+        <nav class="barra">
+            <ul class="items_list">
+               <li class="items_nav"><a href=lista.html>Lista de Gustos</a></li>
+                <li class="items_nav"><a href="../Login.php">Salir</a></li>
+            </ul>
+        </nav>
+    </header>
+    <br>
+
+
+    <form name="f1" id="formulair">
+        <b>
+            <h1 id="titulo">Dessert</h1>
+        </b>
+        <div id="ps">
+            <img src="foto1.jpg" id="IMGPostre">
+            <b>
+                <article>
+                    <h2 id="nombresinho">Waffles Glaseados</h2>
+                </article>
+            </b>
+            <div class="boxes">
+                <input type="button" id="R" class="botones" value="" onclick="rechazar()">
+                <input type="button" id="G" class="botones" value="" onclick="SGustos()">
+                <input type="button" id="SG" class="botones" value="" onclick="gustos()">
+            </div>
+        </div>
+    </form>
     <!-------------------------------------------------PHP------------------------------------------------------------->
  <?php
 $pass=[
