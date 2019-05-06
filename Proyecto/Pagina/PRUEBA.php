2ca4b31b83d1c1810768a491bafa6a4981fba91f
dd <btorres20612@universidadean.edu.co>
2019-05-06 16:46:50 -0500

    kjk

--- Proyecto/Pagina/PRUEBA.php
+++ Proyecto/Pagina/PRUEBA.php
@@ -1,57 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
-    <link rel="stylesheet" href="css/estiloB.css" type="text/css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="App/Page.js"></script>
-
+    <title>Document</title>
 </head>
+<body>
+    <h1><?php echo 'Pagina en php'; ?></h1>
+    <h2>Hola cliente</h2>
 
-    <body id="Login">
-     <!-- navbar -->
-    <header class="nav_bar fixed-top">
+    <p>Este es un ejemplo básico de PHP</p>
 
-        <div class="menu_bar">
-            <p href="#" class="boton_menu"><span class="icon-menu"></span>Menu</p>
-        </div>
-
-        <nav class="barra">
-            <ul class="items_list">
-                <li class="items_nav"><a href=index.php>Inicio</a></li>
-                <li class="items_nav"><a href=AcercaDe.php>Acerca De</a></li>
-                <li class="items_nav"><a href=Team.php>Equipo</a></li>
-                <li class="items_nav"><a href=Contacto.php>Contacto</a></li>
-                <li class="items_nav"><a href=Login.php>Ingresar</a></li>
-            </ul>
-        </nav>
-    </header>
-    <br><br><br>
+    <br>
 
     <form action="respuesta.php" method="get">
-        <input class="aa" id="usser"type="text" placeholder="&#128272; Usuario" name="Usuario">
-        <input class="aa" name="ass" id="pass" type="password" placeholder="&#128273; Contraseña" name="clave" >
-        <button type="submit" class="BIngresar" id="validateButton" value="Enviar" >Ingresar</button>
+        <input type="text" name="usser" id="usser" placeholder="Usuario">
+        <input type="password" name="pass" id="pass" placeholder="Contraseña">
+        <input type="submit" value="Enviar">
     </form>
-
-    <br><br><br><br><br><br><br><br><br><br><br>
-    </body>
-    <footer class="footer">
-        E-mail: <b>btorres20612@universidadean.edu.co</b> and <b>Jsanche03060@universidadean.edu.co</b><br>
-        Aviso de privacidad |Política de privacidad | Manual de Cookies u otras Tecnologías
-    </footer>
-     <!-- --------------------------------------Php------->
-
-   <?php
-        if (isset($_COOKIE['test'])){
-            echo '<p>Hay Cookie</p>';
-            setcookie('test', '', time() - 3600);
-        }else {
-            echo '<p>No hay Cookie</p>';
-            setcookie('test', 'mi asombrosa cookie' );
-        }
-    ?>
-
+</body>
 </html>
