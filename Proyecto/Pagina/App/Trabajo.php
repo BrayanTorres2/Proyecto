744b3105efdd848645b6746d63b3c08157333365
dd <btorres20612@universidadean.edu.co>
2019-05-06 14:50:54 -0500

    cambios opop

--- Proyecto/Pagina/App/Trabajo.php
+++ Proyecto/Pagina/App/Trabajo.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="App/AppC.css" type="text/css">
-    <script type="text/javascript" src="App/Page.js"></script>
+    <link rel="stylesheet" href="AppC.css" type="text/css">
+    <script type="text/javascript" src="Page.js"></script>
     <title>App</title>
 </head>
 
@@ -19,7 +19,7 @@
 
         <nav class="barra">
             <ul class="items_list">
-               <li class="items_nav"><a href=lista.html>Inicio</a></li>
+               <li class="items_nav"><a href=lista.html>Lista de Gustos</a></li>
                 <li class="items_nav"><a href="../Login.php">Salir</a></li>
             </ul>
         </nav>
