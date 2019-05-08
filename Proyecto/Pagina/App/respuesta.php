518b131b7e5073df2e1c1b6ca6c7c8cd7bf8ad3e
dd <btorres20612@universidadean.edu.co>
2019-05-08 13:46:32 -0500

    fgh

--- Proyecto/Pagina/App/respuesta.php
+++ Proyecto/Pagina/App/respuesta.php
@@ -61,7 +61,7 @@
             </b>
             <div class="boxes">
 
-            <form action="mostrar_listas_gustos.php" method="get">
+            <form action="" method="get">
                 <input type="submit" id="R" class="botones" value="" onclick="rechazar()">
                 <input type="submit" id="G" class="botones" value="" onclick="SGustos()">
                 <input type="submit" id="SG" class="botones" value="" onclick="gustos()">
