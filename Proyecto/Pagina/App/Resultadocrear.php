da23cf348f456c74627dca1a9107fa7aa4e5612b
Fabian Meza <fmezacr96407@universidadean.edu.co>
2019-06-10 13:49:57 -0500

    123123

--- Proyecto/Pagina/App/Resultadocrear.php
+++ Proyecto/Pagina/App/Resultadocrear.php
@@ -7,11 +7,14 @@ $db = 'carpetitaProyecto';
 $conexion = mysqli_connect('localhost', 'server', 'server', 'carpetitaProyecto') or die ("No se ha podido conectar al servidor de Base de datos");
 
 /* insertar datos en la base de datos */
+    $userDB=$_POST['Nombreusuario'];
+    $contra=$_POST['Contrasena'];
+    $post="";
 
      if ($userDB=="" || $contra=="" || $email=""){
         echo 'alert("Datos incorrectos o incompletos")';
     }
-        $sql= "INSERT INTO Usuarios (Nombreusuario,Contrasena,Correoelectronico) VALUES ('"$_POST['Nombreusuario']"','"$_POST['Contrasena']"','".$_POST['Correoelectronico']."')";
+        $sql= "INSERT INTO Usuarios (Nombreusuario,Contrasena,Correoelectronico) VALUES ('".$user."','".$contra."','".$_POST['Correoelectronico']."')";
 
 
          if (mysqli_query($conexion, $sql) === TRUE) {
@@ -23,7 +26,7 @@ $conexion = mysqli_connect('localhost', 'server', 'server', 'carpetitaProyecto')
             echo 'alert("Error")';
 
         }
-        $conexion ->close();
+        mysqli_close( $conexion );
 
 ?>
 
