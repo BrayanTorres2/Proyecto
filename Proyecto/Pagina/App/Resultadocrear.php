cfa281358c8ad75fa603aeae328c5df8bb807ab9
Fabian Meza <fmezacr96407@universidadean.edu.co>
2019-06-10 14:21:17 -0500

    coneclogibn

--- Proyecto/Pagina/App/Resultadocrear.php
+++ Proyecto/Pagina/App/Resultadocrear.php
@@ -1,16 +1,5 @@
 <?php
-/*Conexion a la base dedatos */
-$host = 'localhost';
-$user = 'server';
-$password = 'server';
-$db = 'carpetitaProyecto';
-$conexion = mysqli_connect('localhost', 'server', 'server', 'carpetitaProyecto') or die ("No se ha podido conectar al servidor de Base de datos");
-if (!$conexion) {
-      die("Connection failed: " . mysqli_connect_error());
-}
-
-echo "Connected successfully";
-
+include ('Conexion.php')
 /* insertar datos en la base de datos */
     $userDB=$_POST['Nombreusuario'];
     $contra=$_POST['Contrasena'];
--- Proyecto/Pagina/App/respuesta.php
+++ Proyecto/Pagina/App/respuesta.php
@@ -1,11 +1,9 @@
 <?php
+include ('Conexion.php')
 session_start();
 
 $nombre = $_POST['Nombreusuario'];
 $password = $_POST['Contrasena'];
-
-		require_once 'Resultadocrear.php';
-  $conn = dbConnect();
 
 
 $consulta = mysqli_query ($conn, "SELECT * FROM sesion WHERE user = '$nombre' AND pass = '$password'");
